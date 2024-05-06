3ebc9050a8f0881190dbb7c89cba52af595823e3
Petr Kočí <petr.koci@samizdat.cz>
2024-05-06 12:40:03 +0200

    skeleton, germany divided

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className={"font-bold text-lg"}>ahoj</h2>
+    </>
+  )
+}
+
+export default App
