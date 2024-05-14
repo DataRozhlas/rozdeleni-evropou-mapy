7b7e8f8668255577d39252fd7c85a521f66f707f
Petr Kočí <petr.koci@samizdat.cz>
2024-05-14 23:16:03 +0200

    data

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rozdeleni-evropou-mapy/",
   plugins: [react()],
   resolve: {
     alias: {
